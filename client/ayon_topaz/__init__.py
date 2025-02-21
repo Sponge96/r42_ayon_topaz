d6d210ec7548820302a57d714163b23b598c249c
Jack P <jack.p@random42.com>
2025-02-21 12:28:01 +0000

    horrible but working prototype

--- client/ayon_topaz/__init__.py
+++ client/ayon_topaz/__init__.py
@@ -2,6 +2,7 @@ from qtpy import QtWidgets
 import ayon_api
 
 from ayon_core.addon import AYONAddon, click_wrap
+from ayon_core.pipeline import anatomy
 from .version import __version__
 import subprocess
 import os
@@ -109,53 +110,97 @@ def upscale_4k(project, entity_id):
         project_name=project, version_ids=[entity_id]
     )
     target = None
+    # we need to fetch the correct rep not just using mov this is error prone
     for rep in representations:
-        if rep.get("context").get("ext") == "mov":
-            target = rep
+        if rep.get("data"):
+            if rep.get("data").get("context"):
+                if rep.get("data").get("context").get("output") == "ProRes":
+                    target = rep
 
     if target:
-        input = os.path.normpath(target.get("attrib").get("path"))
-        output = os.path.normpath("Z:/_TEMP/Jack.P/test_area/test.mov")
+        try:
+            input = get_input(target)
+            data = target.get("data")
+            format_data = {"data": data["context"]}
+            format_data["data"]["output"] = "4k"
+            format_data["data"]["representation"] = "4k"
+            template_data = get_template_data(con, project, format_data)
+            output = template_data["path"]
+        except Exception as e:
+            app = QtWidgets.QApplication()
+            QtWidgets.QMessageBox.information(
+                None,
+                "Triggered from AYON Server",
+                f"'{e}'",
+            )
 
-        command = [
-            "ffmpeg",
-            "-hide_banner",
-            "-i",
-            input,
-            "-sws_flags",
-            "spline+accurate_rnd+full_chroma_int",
-            "-color_trc",
-            "2",
-            "-colorspace",
-            "2",
-            "-color_primaries",
-            "2",
-            "-filter_complex",
-            "tvai_up=model=ghq-5:scale=0:w=3840:h=2160:device=0:vram=1:instances=1,scale=w=3840:h=2160:flags=lanczos:threads=0",
-            "-c:v",
-            "prores_ks",
-            "-profile:v",
-            "3",
-            "-vendor",
-            "apl0",
-            "-quant_mat",
-            "hq",
-            "-bits_per_mb",
-            "1350",
-            "-pix_fmt",
-            "yuv422p10le",
-            "-an",
-            "-map_metadata",
-            "0",
-            "-map_metadata:s:v",
-            "0:s:v",
-            "-movflags",
-            "use_metadata_tags+write_colr",
-            "-metadata",
-            "videoai=Enhanced using ghq-5. Changed resolution to 3840x2160",
-            output,
-        ]
-        upscale(command)
+    try:
+        result = ayon_api.create_representation(
+            project_name=project,
+            name="4k__",
+            version_id=entity_id,
+            files=[{"id": ayon_api.utils.create_entity_id(), "path": output}],
+            data=data,
+            attrib={"path": output},
+        )
+    except Exception as e:
+        app = QtWidgets.QApplication()
+        QtWidgets.QMessageBox.information(
+            None,
+            "Triggered from AYON Server",
+            f"'{e}'",
+        )
+
+
+# command = [
+#     "ffmpeg",
+#     "-hide_banner",
+#     "-i",
+#     input,
+#     "-sws_flags",
+#     "spline+accurate_rnd+full_chroma_int",
+#     "-color_trc",
+#     "2",
+#     "-colorspace",
+#     "2",
+#     "-color_primaries",
+#     "2",
+#     "-filter_complex",
+#     "tvai_up=model=ghq-5:scale=0:w=3840:h=2160:device=0:vram=1:instances=1,scale=w=3840:h=2160:flags=lanczos:threads=0",
+#     "-c:v",
+#     "prores_ks",
+#     "-profile:v",
+#     "3",
+#     "-vendor",
+#     "apl0",
+#     "-quant_mat",
+#     "hq",
+#     "-bits_per_mb",
+#     "1350",
+#     "-pix_fmt",
+#     "yuv422p10le",
+#     "-an",
+#     "-map_metadata",
+#     "0",
+#     "-map_metadata:s:v",
+#     "0:s:v",
+#     "-movflags",
+#     "use_metadata_tags+write_colr",
+#     "-metadata",
+#     "videoai=Enhanced using ghq-5. Changed resolution to 3840x2160",
+#     output,
+# ]
+# upscale(command)
+
+
+def get_input(rep):
+    return os.path.normpath(rep.get("attrib").get("path"))
+
+
+def get_template_data(con, project_name, format_data):
+    project_anatomy = anatomy.Anatomy(project_name)
+    template = project_anatomy.get_template_item("publish", "topazUpscale")
+    return template.format(**format_data)
 
 
 def upscale(command):
